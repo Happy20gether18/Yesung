ffdf220400b61c03dc9ef9944038c17bd92b91da
Happy20gether18 <lillianwang.s@gmail.com>
2026-08-20 10:09:49 -0400

    Refresh optimized image cache safely

--- quiz-data.js
+++ quiz-data.js
@@ -4,152 +4,152 @@ window.QUIZ_DATA = Object.freeze({
   secondsPerQuestion: 5,
   questions: [
     {
-      image: "images/01.webp",
+      image: "images/01.webp?v=20260820",
       answer: "Together",
       options: ["Floral Sense", "Together", "Beautiful Night", "Small Things"]
     },
     {
-      image: "images/02.webp",
+      image: "images/02.webp?v=20260820",
       answer: "春天的阵雨",
       options: ["春天的阵雨", "Unfading Sense", "Curtain", "Easy"]
     },
     {
-      image: "images/03.webp",
+      image: "images/03.webp?v=20260820",
       answer: "Scented Things",
       options: ["Whatcha Doin", "Phantom Pain", "Scented Things", "Floral Sense"]
     },
     {
-      image: "images/04.webp",
+      image: "images/04.webp?v=20260820",
       answer: "Pink Magic",
       options: ["Beautiful Paradox", "There She Goes Again", "Easy", "Pink Magic"]
     },
     {
-      image: "images/05.webp",
+      image: "images/05.webp?v=20260820",
       answer: "Because I Love You ～大切な绊～",
       options: ["束の间の恋", "Because I Love You ～大切な绊～", "写しかがみ～憎しみの矛先～", "咲き夸る时を待つのは"]
     },
     {
-      image: "images/06.webp",
+      image: "images/06.webp?v=20260820",
       answer: "Here I am",
       options: ["Corazón Perdido (Lost Heart)", "It's Complicated", "Here I am", "Beautiful Night"]
     },
     {
-      image: "images/07.webp",
+      image: "images/07.webp?v=20260820",
       answer: "It's Complicated",
       options: ["It's Complicated", "Beautiful Paradox", "Phantom Pain", "Easy"]
     },
     {
-      image: "images/08.webp",
+      image: "images/08.webp?v=20260820",
       answer: "There She Goes Again",
       options: ["Whatcha Doin", "There She Goes Again", "Curtain", "Beautiful Night"]
     },
     {
-      image: "images/09.webp",
+      image: "images/09.webp?v=20260820",
       answer: "束の间の恋",
       options: ["写しかがみ～憎しみの矛先～", "咲き夸る时を待つのは", "束の间の恋", "Because I Love You ～大切な绊～"]
     },
     {
-      image: "images/10.webp",
+      image: "images/10.webp?v=20260820",
       answer: "写しかがみ～憎しみの矛先～",
       options: ["春天的阵雨", "束の间の恋", "咲き夸る时を待つのは", "写しかがみ～憎しみの矛先～"]
     },
     {
-      image: "images/11.webp",
+      image: "images/11.webp?v=20260820",
       answer: "咲き夸る时を待つのは",
       options: ["咲き夸る时を待つのは", "写しかがみ～憎しみの矛先～", "Because I Love You ～大切な绊～", "束の间の恋"]
     },
     {
-      image: "images/12.webp",
+      image: "images/12.webp?v=20260820",
       answer: "Small Things",
       options: ["Floral Sense", "Small Things", "Scented Things", "Unfading Sense"]
     },
     {
-      image: "images/13.webp",
+      image: "images/13.webp?v=20260820",
       answer: "Phantom Pain",
       options: ["Beautiful Paradox", "Corazón Perdido (Lost Heart)", "Phantom Pain", "It's Complicated"]
     },
     {
-      image: "images/14.webp",
+      image: "images/14.webp?v=20260820",
       answer: "Floral Sense",
       options: ["Scented Things", "Unfading Sense", "Small Things", "Floral Sense"]
     },
     {
-      image: "images/15.webp",
+      image: "images/15.webp?v=20260820",
       answer: "Corazón Perdido (Lost Heart)",
       options: ["Corazón Perdido (Lost Heart)", "There She Goes Again", "Phantom Pain", "Beautiful Paradox"]
     },
     {
-      image: "images/16.webp",
+      image: "images/16.webp?v=20260820",
       answer: "No More Love",
       options: ["No More Love", "It's Complicated", "There She Goes Again", "Easy"]
     },
     {
-      image: "images/17.webp",
+      image: "images/17.webp?v=20260820",
       answer: "雨のち晴れの空の色",
       options: ["束の间の恋", "雨のち晴れの空の色", "咲き夸る时を待つのは", "写しかがみ～憎しみの矛先～"]
     },
     {
-      image: "images/18.webp",
+      image: "images/18.webp?v=20260820",
       answer: "Splash",
       options: ["Floral Sense", "Small Things", "Splash", "Scented Things"]
     },
     {
-      image: "images/19.webp",
+      image: "images/19.webp?v=20260820",
       answer: "Whatcha Doin",
       options: ["Beautiful Night", "There She Goes Again", "No More Love", "Whatcha Doin"]
     },
     {
-      image: "images/20.webp",
+      image: "images/20.webp?v=20260820",
       answer: "いま会いにゆきます 〜IF you〜",
       options: ["いま会いにゆきます 〜IF you〜", "雨のち晴れの空の色", "束の间の恋", "咲き夸る时を待つのは"]
     },
     {
-      image: "images/21.webp",
+      image: "images/21.webp?v=20260820",
       answer: "Beautiful Night",
       options: ["Beautiful Night", "Beautiful Paradox", "Together", "No More Love"]
     },
     {
-      image: "images/22.webp",
+      image: "images/22.webp?v=20260820",
       answer: "MONOLOGUE",
       options: ["My Dear", "MONOLOGUE", "Carpet", "Curtain"]
     },
     {
-      image: "images/23.webp",
+      image: "images/23.webp?v=20260820",
       answer: "Darling U",
       options: ["Slide Away", "My Dear", "Darling U", "Spring in Me"]
     },
     {
-      image: "images/24.webp",
+      image: "images/24.webp?v=20260820",
       answer: "Curtain",
       options: ["Carpet", "Floral Sense", "Phantom Pain", "Curtain"]
     },
     {
-      image: "images/25.webp",
+      image: "images/25.webp?v=20260820",
       answer: "Slide Away",
       options: ["Slide Away", "No More Love", "Splash", "Easy"]
     },
     {
-      image: "images/26.webp",
+      image: "images/26.webp?v=20260820",
       answer: "もしかしたら仆の物语 ～そして仆ら～",
       options: ["雨のち晴れの空の色", "もしかしたら仆の物语 ～そして仆ら～", "いま会いにゆきます 〜IF you〜", "咲き夸る时を待つのは"]
     },
     {
-      image: "images/27.webp",
+      image: "images/27.webp?v=20260820",
       answer: "Carpet",
       options: ["Curtain", "My Dear", "Carpet", "Darling U"]
     },
     {
-      image: "images/28.webp",
+      image: "images/28.webp?v=20260820",
       answer: "My Dear",
       options: ["Darling U", "MONOLOGUE", "Small Things", "My Dear"]
     },
     {
-      image: "images/29.webp",
+      image: "images/29.webp?v=20260820",
       answer: "Still Standing",
       options: ["Still Standing", "春天的阵雨", "Floral Sense", "Scented Things"]
     },
     {
-      image: "images/30.webp",
+      image: "images/30.webp?v=20260820",
       answer: "Hibernation 冬眠",
       options: ["束の间の恋", "Hibernation 冬眠", "雨のち晴れの空の色", "いま会いにゆきます 〜IF you〜"]
     }
